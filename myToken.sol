632b8b79b1ba24b4479ff67dd8f29a78c49d6183
gaidom <134861578+gaidom@users.noreply.github.com>
2023-06-15 16:57:56 +0800

    Updated myToken.sol

--- myToken.sol
+++ myToken.sol
@@ -26,14 +26,16 @@ contract MyToken {
     mapping(address => uint) public balances;
 
     // mint function
-    function mint (address _address, uint _value) public{
+    function mint (address _address, uint _value) public {
         totalSupply += _value;
         balances[_address] += _value;
+    }
 
     // burn function
-    function burn (address _address, uint _value) public{
-        if (balances[_address] >= _value){
+    function burn (address _address, uint _value) public { 
+        if (balances[_address] >= _value) {
         totalSupply -= _value;
         balances[_address] -= _value;
-       }
-   }
+        }
+    }
+}
